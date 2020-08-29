19dd517dd4ba806af6baaa884ea91066de6c2c6c
Thomas <Thomasbronsveld94@gmail.com>
2020-08-29 18:34:43 +0200

    FizzBuzz challenge added

---
+++ java/fizzbuzz/src/main/java/Fizzbuzz.java
@@ -0,0 +1,18 @@
+package main.java;
+
+public class Fizzbuzz {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            if( (i % 15 == 0) ) {
+                System.out.print("FizzBuzz ");
+            } else if( (i % 5 == 0)) {
+                System.out.print("Buzz ");
+            } else if( (i % 3 == 0)) {
+                System.out.print("Fizz ");
+
+            } else {
+                System.out.println(i + " ");
+            }
+        }
+    }
+}
